b294f528b2ffc75dcb5ed9d3cc3e652d57e62d5c
Антон Тимонин <antontimonin@MacBook-Pro-od-Anton.local>
2019-11-30 00:44:26 +0300

    Uploaded singleton swift language

---
+++ Creational Patterns/Singleton/Singleton.swift
@@ -0,0 +1,21 @@
+
+class Singleton1 {
+    
+    private init() {}
+    
+    private static var uniqueValue: Singleton1?
+    
+    public func get_instance() -> Singleton1 {
+        if Singleton1.uniqueValue == nil {
+            Singleton1.uniqueValue = Singleton1()
+        }
+        return Singleton1.uniqueValue!
+    }
+}
+
+class Singleton {
+    
+    private init() {}
+    
+    private static let uniqueValue = Singleton()
+}
