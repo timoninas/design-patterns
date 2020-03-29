2b3a988a1150ca459d7f5413f4bb9e26f207da9c
Anton Timonin <antonandegor@mail.ru>
2020-03-29 14:36:21 +0300

    Observer introduction

---
+++ Behavioral Patterns/Observer/Observer.swift
@@ -0,0 +1,74 @@
+
+import UIKit
+import Foundation
+var str = "Hello, playground"
+
+protocol Observer: class {
+    func update()
+}
+
+class Notifier {
+    var state: Int = {
+        return Int(arc4random_uniform(100))
+    }()
+    
+    private lazy var observers = [Observer]()
+    
+    func add(_ subject: Observer) {
+        print(#function)
+        observers.append(subject)
+        
+    }
+    
+    func remove(_ subject: Observer) {
+        if let index = observers.firstIndex(where: {$0 === subject}) {
+            observers.remove(at: index)
+            print(#function)
+        }
+        
+    }
+    
+    func notify() {
+        print(#function)
+        for value in observers {
+            value.update()
+        }
+    }
+    
+    func businessLogic() {
+        print(#function)
+        print("Lol kek businessLogic cheburek ⚗️")
+        notify()
+    }
+}
+
+class Observer1: Observer {
+    func update() {
+        print("Observer1, DO WATCH")
+    }
+}
+
+class Observer2: Observer {
+    func update() {
+        print("Observer2, play in Metro Exodus")
+    }
+}
+
+func main() {
+    let notifier = Notifier()
+    let obs1 = Observer1()
+    let obs2 = Observer2()
+    
+    notifier.add(obs1)
+    notifier.businessLogic()
+    
+    print("\n----- SEPARATOR -----\n")
+    
+    notifier.remove(obs1)
+    notifier.add(obs2)
+    notifier.add(obs1)
+    notifier.businessLogic()
+}
+
+main()
+
