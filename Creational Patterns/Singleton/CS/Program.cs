7cb7d823a876488475f1922b64a57aca341d7166
Timonin Anton <dotruger.37@gmail.com>
2020-07-09 23:01:45 +0300

    add c sharp realisation singleton

---
+++ Creational Patterns/Singleton/CS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton tmp1;
+            Singleton tmp2;
+
+            tmp1 = Singleton.getInstance("Singleton pattern");
+            tmp2 = Singleton.getInstance("NOT Singleton pattern");
+
+            Console.WriteLine(tmp1.namePattern);
+            Console.WriteLine(tmp2.namePattern);
+        }
+    }
+}
---
+++ Creational Patterns/Singleton/CS/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        private static Singleton instance;
+        public string namePattern { get; private set; }
+        protected Singleton(string name) 
+        {
+            this.namePattern = name;
+        }
+
+        public static Singleton getInstance(string name)
+        {
+            if (instance == null)
+            {
+                instance = new Singleton(name);
+            }
+
+            return instance;
+        }
+    }
+}
