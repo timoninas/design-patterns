3186995707533006ed59dcd546a14c3ab573465e
Timonin Anton <dotruger.37@gmail.com>
2020-07-12 09:56:38 +0300

    add C# realisation of pattern Prototype

---
+++ Creational Patterns/Prototype/CS/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(10);
+            Circle circle2;
+
+            circle2 = (Circle)circle1.clone();
+
+            circle1.GetInfo();
+            circle2.GetInfo();
+
+
+            Rectangle rectangle1 = new Rectangle(10, 3);
+            Rectangle rectangle2;
+
+            rectangle2= (Rectangle)rectangle1.clone();
+
+            rectangle1.GetInfo();
+            rectangle2.GetInfo();
+
+            /* OUTPUT
+             * Shape of this Circle: 10
+             * Shape of this Circle: 10
+             * Shape of this Rectangle: 2x3
+             * Shape of this Rectangle: 2x3
+             */
+        }
+    }
+}
---
+++ Creational Patterns/Prototype/CS/Prototype.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    interface IFigure
+    {
+        public IFigure clone();
+        public double GetShapeArea();
+        public void GetInfo();
+    }
+    class Circle : IFigure
+    {
+        private uint _radiusCircle;
+        public Circle(uint radius)
+        {
+            this._radiusCircle = radius;
+        }
+        public IFigure clone()
+        {
+            return new Circle(this._radiusCircle);
+        }
+
+        public double GetShapeArea()
+        {
+            return 3.14 * this._radiusCircle * this._radiusCircle;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Shape of this Circle: " +  this._radiusCircle.ToString());
+        }
+    }
+
+    class Rectangle: IFigure
+    {
+        private uint _w, _h;
+
+        public Rectangle(uint w, uint h)
+        {
+            this._w = 2;
+            this._h = h;
+        }
+
+        public IFigure clone()
+        {
+            return new Rectangle(this._w, this._h);
+        }
+
+        public double GetShapeArea()
+        {
+            return this._w * this._h;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Shape of this Rectangle: " + this._w + "x" + this._h);
+        }
+    }
+}
