82c8d089e71d9c9b5ebdef46a97598b278273cdd
Timonin Anton <dotruger.37@gmail.com>
2020-07-22 12:55:04 +0300

    Add c# realization for Abstracto Factory patterm

---
+++ Creational Patterns/Abstract factory/CS/AbstractFacory.cs
@@ -0,0 +1,118 @@
+﻿using System;
+namespace AbstractFactory
+{
+    public abstract class AbstractFactory
+    {
+        public abstract Application CreateMobileApp();
+
+        public abstract Application CreateWebApp();
+    }
+
+    public class CompanyRegedit : AbstractFactory
+    {
+        public override Application CreateMobileApp()
+        {
+            return new MobileApplicationForRegedit("Regedit App", MobileOS.iOS);
+        }
+
+        public override Application CreateWebApp()
+        {
+            return new WebApplicationForRegedit("Regedit Web App", TypeWebSite.adapted);
+        }
+    }
+
+    public class CompanyJukebox: AbstractFactory
+    {
+        public override Application CreateMobileApp()
+        {
+            return new MobileApplicationForJukebox("Jukebox App", MobileOS.Android);
+        }
+
+        public override Application CreateWebApp()
+        {
+            return new WebApplicationForJukebox("Jukebox Web App", TypeWebSite.simple);
+        }
+    }
+
+    public abstract class Application
+    {
+        public string name { get; private set; }
+
+        public Application(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract void OpenApp();
+    }
+
+    public enum MobileOS
+    {
+        iOS, Android
+    }
+
+    public class MobileApplicationForRegedit: Application
+    {
+        public MobileOS typeOS { get; private set; }
+
+        public MobileApplicationForRegedit(string name, MobileOS os): base(name)
+        {
+            this.typeOS = os;
+        }
+
+        public override void OpenApp()
+        {
+            Console.WriteLine($"Regedit create app: {this.name} with os: {this.typeOS}");
+        }
+    }
+
+    public class MobileApplicationForJukebox : Application
+    {
+        public MobileOS TypeOS { get; private set; }
+
+        public MobileApplicationForJukebox(string name, MobileOS os) : base(name)
+        {
+            this.TypeOS = os;
+        }
+
+        public override void OpenApp()
+        {
+            Console.WriteLine($"Jukebox create app: {this.name} with os: {this.TypeOS}");
+        }
+    }
+
+    public enum TypeWebSite
+    {
+        simple, adapted
+    }
+
+    public class WebApplicationForRegedit : Application
+    {
+        public TypeWebSite TypeSite { get; private set; }
+
+        public WebApplicationForRegedit(string name, TypeWebSite browser) : base(name)
+        {
+            this.TypeSite = browser;
+        }
+
+        public override void OpenApp()
+        {
+            Console.WriteLine($"Regedit create web app: {this.name} with type: {this.TypeSite}");
+        }
+    }
+
+    public class WebApplicationForJukebox : Application
+    {
+        public TypeWebSite TypeSite { get; private set; }
+
+        public WebApplicationForJukebox(string name, TypeWebSite browser) : base(name)
+        {
+            this.TypeSite = browser;
+        }
+
+        public override void OpenApp()
+        {
+            Console.WriteLine($"Jukebox create web app: {this.name} with type: {this.TypeSite}");
+        }
+    }
+}
---
+++ Creational Patterns/Abstract factory/CS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbstractFactory
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            CompanyJukebox company1 = new CompanyJukebox();
+            CompanyRegedit company2 = new CompanyRegedit();
+
+            List<Application> apps = new List<Application>();
+
+            apps.Add(company1.CreateMobileApp());
+            apps.Add(company1.CreateWebApp());
+            apps.Add(company2.CreateMobileApp());
+            apps.Add(company2.CreateWebApp());
+            apps.Add(company1.CreateWebApp());
+
+            foreach (var app in apps)
+            {
+                app.OpenApp();
+            }
+        }
+
+        // OUTPUT
+        // Jukebox create web app: Jukebox Web App with type: simple
+        // Regedit create app: Regedit App with os: iOS
+        // Regedit create web app: Regedit Web App with type: adapted
+        // Jukebox create web app: Jukebox Web App with type: simple
+    }
+}
