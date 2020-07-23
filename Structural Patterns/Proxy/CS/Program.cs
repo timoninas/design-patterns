8c22dd84bdb2a4a68cac2da83d723f109572e363
Timonin Anton <dotruger.37@gmail.com>
2020-07-24 00:21:04 +0300

    Added c# realization for proxy pattern

---
+++ Structural Patterns/Proxy/CS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ProxyPattern
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            UserStorageProxy proxy = new UserStorageProxy();
+
+            User user1 = proxy.GetConcreteUser(1);
+            User user2 = proxy.GetConcreteUser(1);
+            User user3 = proxy.GetConcreteUser(100);
+
+            Console.WriteLine($"User1 - {user1.id} {user1.username} {user1.password}");
+            Console.WriteLine($"User2 - {user2.id} {user2.username} {user2.password}");
+            if (user3 == null)
+            {
+                Console.WriteLine($"User3 - null null null");
+            } else
+            {
+                Console.WriteLine($"User3 - {user3.id} {user3.username} {user3.password}");
+            }
+
+            // OUTPUT
+            // Accessing the ReceivedUsers
+            // Accessing the UserContext
+            // User1 - 1 login password
+            // User2 - 1 login password
+            // User3 - null null null
+        }
+    }
+}
---
+++ Structural Patterns/Proxy/CS/Proxy.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProxyPattern
+{
+    // Example for Entity Framework
+    // Table User
+    public class User
+    {
+        public int id { get; private set; }
+        public string username { get; private set; }
+        public string password { get; private set; }
+
+        public User(int id, string username, string password)
+        {
+            this.id = id;
+            this.username = username;
+            this.password = password;
+        }
+    }
+
+    // Fake converted  UserContext: DbContext
+    public class dbUser
+    {
+        public  List<User> Users { get; private set; }
+
+        public dbUser()
+        {
+            this.Users = new List<User>();
+
+            this.Users.Add(new User(0, "123", "321"));
+            this.Users.Add(new User(1, "login", "password"));
+            this.Users.Add(new User(2, "It is login", "My password"));
+            this.Users.Add(new User(3, "antontimonin", "urloowl;"));
+            this.Users.Add(new User(4, "tele", "kek"));
+        }
+
+        public User GetConcreteUser(int number)
+        {
+            int index = this.Users.FindIndex(u => u.id == number);
+            if (index == -1)
+            {
+                return null;
+            }
+
+            return this.Users[index];
+         }
+
+        public void Clear()
+        {
+            this.Users.Clear();
+        }
+    }
+
+    // Class for request to db
+    public class UserStorage
+    {
+        public dbUser db { get; set; }
+
+        public UserStorage()
+        {
+            this.db = new dbUser();
+        }
+
+        public User GetConcreteUser(int number)
+        {
+            return this.db.GetConcreteUser(number);
+        }
+
+        public void ClearDB()
+        {
+            this.db.Clear();
+        }
+    }
+
+    // PROXY
+    public class UserStorageProxy
+    {
+        public List<User> ReceivedUsers { get; private set; }
+        public UserStorage userstorage { get; private set; }
+
+        public UserStorageProxy()
+        {
+            this.ReceivedUsers = new List<User>();
+        }
+
+        public User GetConcreteUser(int number) 
+        {
+            User user = this.ReceivedUsers.FirstOrDefault(p => p.id == number);
+
+            if (user == null)
+            {
+                if (this.userstorage == null)
+                {
+                    this.userstorage = new UserStorage();
+                }
+
+                Console.WriteLine("Accessing the UserContext");
+                user = this.userstorage.GetConcreteUser(number);
+                this.ReceivedUsers.Add(user);
+            } else
+            {
+                Console.WriteLine("Accessing the ReceivedUsers");
+            }
+
+            return user;
+        }
+
+        public void ClreadDB()
+        {
+            if (this.userstorage != null)
+            {
+                this.userstorage.ClearDB();
+            }
+        }
+    }
+}
