fe6ca87b22179578ba5d0206d18233bcc8153012
Timonin Anton <dotruger.37@gmail.com>
2020-07-24 22:49:01 +0300

    Add C# realization for Bridge pattern

---
+++ Structural Patterns/Bridge/CS/Bridge.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bridge
+{
+    public class Bridge
+    {
+        public class Item
+        {
+            public int id { get; private set; }
+            public double price { get; private set; }
+            public double weight { get; private set; }
+
+            public Item(int id, double price, double weight)
+            {
+                this.id = id;
+                this.price = price;
+                this.weight = weight;
+            }
+        }
+
+        // ABSTRACTION
+        public abstract class PriceCalculator
+        {
+            protected IPriceItemCalculator_Realisation _impl { get; set; }
+            public List<Item> Items { get; private set; }
+
+            public PriceCalculator(IPriceItemCalculator_Realisation implementation)
+            {
+                this._impl = implementation;
+                this.Items = new List<Item>();
+            }
+
+            public void Add(Item item)
+            {
+                this.Items.Add(item);
+            }
+
+            public abstract double GetTotalPrice();
+        }
+
+        // CONCRETE ABSTRACTION
+        public class ConcretePriceCalculator: PriceCalculator
+        {
+            public ConcretePriceCalculator(IPriceItemCalculator_Realisation implementation):
+                base(implementation) { }
+
+            public override double GetTotalPrice()
+            {
+                double totalPrice = 0;
+
+                if (this.Items.Count > 0)
+                {
+                    foreach (Item item in this.Items)
+                    {
+                        totalPrice += this._impl.GetItemPrice(item);
+                    }
+                }
+
+                return totalPrice;
+            }
+        }
+
+
+
+
+        //REALIZATION
+        public interface IPriceItemCalculator_Realisation
+        {
+            double GetItemPrice(Item item);
+        }
+
+        // CONCRETE REALIZATION 1
+        public class CounterByPrice: IPriceItemCalculator_Realisation
+        {
+            public double GetItemPrice(Item item)
+            {
+                double totalPrice = item.price;
+
+                return totalPrice;
+            }
+        }
+
+        // CONCRETE REALIZATION 2
+        public class CounterByPriceAndWeight : IPriceItemCalculator_Realisation
+        {
+            public double GetItemPrice(Item item)
+            {
+                double totalPrice = item.price + item.weight * 0.1;
+
+                return totalPrice;
+            }
+        }
+    }
+}
---
+++ Structural Patterns/Bridge/CS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static Bridge.Bridge;
+
+namespace Bridge
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            CounterByPrice counterbyprice = new CounterByPrice();
+            CounterByPriceAndWeight counterbyweight = new CounterByPriceAndWeight();
+
+            ConcretePriceCalculator calculator1 = new ConcretePriceCalculator(counterbyprice);
+            ConcretePriceCalculator calculator2 = new ConcretePriceCalculator(counterbyweight);
+
+            calculator1.Add(new Item(0, 20, 30));
+            calculator1.Add(new Item(1, 50, 50));
+            calculator1.Add(new Item(2, 90, 20));
+
+            calculator2.Add(new Item(0, 20, 30));
+            calculator2.Add(new Item(1, 50, 50));
+            calculator2.Add(new Item(2, 90, 20));
+
+            Console.WriteLine($"calculator1 - {calculator1.GetTotalPrice()}");
+            Console.WriteLine($"calculator2 - {calculator2.GetTotalPrice()}");
+
+
+            // OUTPUT
+            // calculator1 - 160
+            // calculator2 - 170
+        }
+    }
+}
