fb69352999bb55bfdc4da0fb7532c7ed717757ed
Timonin Anton <dotruger.37@gmail.com>
2020-08-04 10:15:24 +0300

    Add C# realization pattern Flyweight

---
+++ Structural Patterns/Flyweight/CS/Flyweight.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flyweight
+{
+    public interface IFlyweightFactory
+    {
+        Flyweight GetFlyweight(char symbol);
+    }
+
+    public class FlyweightFactory: IFlyweightFactory
+    {
+        private Dictionary<char, Flyweight> alphabet;
+
+        public FlyweightFactory()
+        {
+            alphabet = new Dictionary<char, Flyweight>();
+        }
+
+        public Flyweight GetFlyweight(char symbol)
+        {
+            if (!this.alphabet.ContainsKey(symbol))
+            {
+                this.alphabet.Add(symbol, new WordFlyweight());
+            }
+
+            return this.alphabet[symbol] as Flyweight;
+        }
+    }
+
+    public abstract class Flyweight
+    {
+        protected static int CountUsedWords = 0;
+
+        public int id { get; private set; }
+
+        public Flyweight()
+        {
+            CountUsedWords++;
+            var random = new Random();
+            this.id = random.Next(0, 1000);
+        }
+
+        public abstract void SomeOperation(string message);
+    }
+
+    public class WordFlyweight: Flyweight
+    {
+        public WordFlyweight(): base() { }
+
+        public override void SomeOperation(string message)
+        {
+            Console.WriteLine($"Message: {message}\n Count used words: {Flyweight.CountUsedWords}");
+        }
+    }
+}
---
+++ Structural Patterns/Flyweight/CS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Flyweight
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            IFlyweightFactory factory = new FlyweightFactory();
+
+            var a = factory.GetFlyweight('a');
+            Console.WriteLine($"{a.id}");
+
+            var b = factory.GetFlyweight('b');
+            Console.WriteLine($"{b.id}");
+
+            var c = factory.GetFlyweight('b');
+            Console.WriteLine($"{c.id}");
+
+            var d = factory.GetFlyweight('d');
+            Console.WriteLine($"{d.id}");
+
+            // 594
+            // 899
+            // 899
+            // 401
+        }
+    }
+}
