9de407190220e41ca9804e56ca2acfede799758a
Timonin Anton <dotruger.37@gmail.com>
2020-08-05 13:37:35 +0300

    Add C# realization pattern Decorator

---
+++ Structural Patterns/Decorator/CS/Decorator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace Decorator
+{
+    public interface IComputer
+    {
+        string GetInfo();
+
+        double GetPrice();
+    }
+
+    public class Computer: IComputer
+    {
+        public string name { get; private set; }
+
+        public Computer(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetInfo()
+        {
+            return name;
+        }
+
+        public double GetPrice()
+        {
+            return 500;
+        }
+    }
+
+    public abstract class ComputerDecorator: IComputer
+    {
+        protected IComputer computer;
+
+        public ComputerDecorator(IComputer computer)
+        {
+            this.computer = computer;
+        }
+
+        public abstract string GetInfo();
+
+        public abstract double GetPrice();
+    }
+
+    public class OperativeMemoryDecorator: ComputerDecorator
+    {
+        public OperativeMemoryDecorator(IComputer computer) : base(computer) { }
+
+        public override string GetInfo()
+        {
+            return computer.GetInfo() + " 8 gb";
+        }
+
+        public override double GetPrice()
+        {
+            return computer.GetPrice() + 100;
+        }
+    }
+
+    public class VideocardDecorator : ComputerDecorator
+    {
+        public VideocardDecorator(IComputer computer) : base(computer) { }
+
+        public override string GetInfo()
+        {
+            return computer.GetInfo() + "  Videocard GTX";
+        }
+
+        public override double GetPrice()
+        {
+            return computer.GetPrice() + 250;
+        }
+    }
+}
---
+++ Structural Patterns/Decorator/CS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Decorator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            IComputer computer = new Computer("Igronator");
+            IComputer computerO = new OperativeMemoryDecorator(computer);
+            IComputer computerVV = new VideocardDecorator(new VideocardDecorator(computer));
+            IComputer computerOVV = new VideocardDecorator(new VideocardDecorator(computerO));
+
+            Console.WriteLine($"{computer.GetInfo()} - {computer.GetPrice()}");
+            Console.WriteLine($"{computerO.GetInfo()} - {computerO.GetPrice()}");
+            Console.WriteLine($"{computerVV.GetInfo()} - {computerVV.GetPrice()}");
+            Console.WriteLine($"{computerOVV.GetInfo()} - {computerOVV.GetPrice()}");
+
+            // OUTPUT
+            // Igronator - 500
+            // Igronator 8 gb - 600
+            // Igronator Videocard GTX Videocard GTX - 1000
+            // Igronator 8 gb Videocard GTX Videocard GTX - 1100
+        }
+    }
+}
