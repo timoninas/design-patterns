31cb86406ad8d1307c2a9d2fa551765d1f574c49
Timonin Anton <dotruger.37@gmail.com>
2020-08-06 10:46:06 +0300

    Add C# realization for pattern Composite

---
+++ Structural Patterns/Composite/CS/Composite.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Composite
+{
+    public interface Composite
+    {
+        string Name { get; }
+        void AddComponent(Composite component);
+        void ShowComponents();
+    }
+
+    public abstract class FileSystemObject: Composite
+    {
+        public string Name { get; }
+
+        public FileSystemObject(string name)
+        {
+            this.Name = name;
+        }
+
+        public abstract void AddComponent(Composite component);
+        public abstract void ShowComponents();
+    }
+
+    public class File: FileSystemObject
+    {
+        public File(string name) : base(name) { }
+
+        public override void AddComponent(Composite component)
+        {
+            throw new Exception("You cannot add object in file");
+        }
+
+        public override void ShowComponents()
+        {
+            Console.WriteLine($"File > {this.Name}");
+        }
+    }
+
+    public class Directory : FileSystemObject
+    {
+        public List<Composite> InnerObjects { get; private set; }
+
+        public Directory(string name) : base(name)
+        {
+            this.InnerObjects = new List<Composite>();
+        }
+
+        public override void AddComponent(Composite component)
+        {
+            if (component != null)
+            {
+                this.InnerObjects.Add(component);
+            }
+        }
+
+        public override void ShowComponents()
+        {
+            Console.WriteLine($"Dir  > {this.Name}");
+            foreach (var item in this.InnerObjects)
+            {
+                item.ShowComponents();
+            }
+        }
+    }
+}
---
+++ Structural Patterns/Composite/CS/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Composite
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Composite dirInit = new Directory("Initial dir");
+            Composite dir1 = new Directory("Folder 1");
+            Composite dir2 = new Directory("Folder 2");
+            Composite dir3 = new Directory("Folder 3");
+            Composite dir4 = new Directory("Folder 4");
+
+            Composite file1 = new File("gol.obj");
+            Composite file2 = new File("resume.docx");
+            Composite file3 = new File("lab1.c");
+            Composite file4 = new File("lab1.h");
+            Composite file5 = new File("lab1.obj");
+
+            dir4.AddComponent(file3);
+            dir4.AddComponent(file4);
+            dir4.AddComponent(file5);
+
+            dir3.AddComponent(dir2);
+
+            dir2.AddComponent(dir4);
+
+            dir1.AddComponent(file2);
+
+            dirInit.AddComponent(file1);
+            dirInit.AddComponent(dir1);
+            dirInit.AddComponent(dir3);
+
+            dirInit.ShowComponents();
+
+            // OUTPUT
+            // Dir > Initial dir
+            // File > gol.obj
+            // Dir > Folder 1
+            // File > resume.docx
+            // Dir > Folder 3
+            // Dir > Folder 2
+            // Dir > Folder 4
+            // File > lab1.c
+            // File > lab1.h
+            // File > lab1.obj
+        }
+    }
+}
