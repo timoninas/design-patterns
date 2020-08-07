63f228d7d9ba55cf0723cf7beb019515eef1b4f6
Anton Timonin <dotruger.37@gmail.com>
2020-08-07 13:52:48 +0300

    add console output

--- Behavioral Patterns/Iterator/CS/Program.cs
+++ Behavioral Patterns/Iterator/CS/Program.cs
@@ -19,5 +19,16 @@ namespace Iterator
                 Console.WriteLine($"{iterator.Next()}"); 
             }
         }
+        
+        // OUTPUT
+        // 8
+        // 7
+        // 6
+        // 5
+        // 4
+        // 3
+        // 2
+        // 1
+        // 0
     }
 }
