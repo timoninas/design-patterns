8eaceae3b95acf1c7f32fb99b05f6841fd413934
Timonin Anton <dotruger.37@gmail.com>
2020-08-09 14:44:52 +0300

    Add C# realization

---
+++ Behavioral Patterns/Mediator/CS/Mediator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace Mediator
+{
+    // MEDIATOR
+    public interface IMediator
+    {
+        void sendMessage(string message, Printer part);
+    }
+
+    public class PrinterMediator: IMediator
+    {
+        public PrinterSwitcher switcher { get; set; }
+        public PrinterPrinthead printhead { get; set; }
+        public PrinterPaintExtractor extractor { get; set; }
+
+        public void sendMessage(string message, Printer part)
+        {
+            if (switcher == part)
+            {
+                printhead.Notify(message);
+            }
+            else if (printhead == part)
+            {
+                extractor.Notify(message);
+            }
+            else if (extractor == part)
+            {
+                switcher.Notify(message);
+            }
+        }
+    }
+
+    public abstract class Printer
+    {
+        private IMediator mediator;
+
+        public Printer(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public void SendMessage(string message)
+        {
+            mediator.sendMessage(message, this);
+        }
+
+        public abstract void Notify(string message);
+    }
+
+    public class PrinterSwitcher: Printer
+    {
+        public PrinterSwitcher(IMediator mediator) : base(mediator) { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine($"Switcher: {message}");
+        }
+    }
+
+    public class PrinterPrinthead : Printer
+    {
+        public PrinterPrinthead(IMediator mediator) : base(mediator) { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine($"Printhead: {message}");
+        }
+    }
+
+    public class PrinterPaintExtractor : Printer
+    {
+        public PrinterPaintExtractor(IMediator mediator) : base(mediator) { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine($"PaintExtractor: {message}");
+        }
+    }
+}
---
+++ Behavioral Patterns/Mediator/CS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Mediator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            PrinterMediator mediator = new PrinterMediator();
+
+            PrinterSwitcher switcher = new PrinterSwitcher(mediator);
+            PrinterPrinthead printhead = new PrinterPrinthead(mediator);
+            PrinterPaintExtractor extractor = new PrinterPaintExtractor(mediator);
+
+            mediator.switcher = switcher;
+            mediator.printhead = printhead;
+            mediator.extractor = extractor;
+
+            Console.OutputEncoding = Encoding.UTF8;
+            switcher.SendMessage("Запуск печатающей головки");
+            printhead.SendMessage("Распылить краску на бумагу");
+            extractor.SendMessage("Закончить распечатывать");
+        }
+
+    }
+}
