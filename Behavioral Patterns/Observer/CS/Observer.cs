52d44dbb90a83645f7e63281d5a13cb6efb98a8f
Timonin Anton <dotruger.37@gmail.com>
2020-08-10 10:16:20 +0300

    Add C# realization Observer

---
+++ Behavioral Patterns/Observer/CS/Observer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+    // СМОТРЯЩИЙ
+    public interface IObserver
+    {
+        void Update();
+    }
+
+    // ОБОЗРИМЫЙ
+    public interface IObservable
+    {
+        void AddObserver(IObserver observer);
+        void RemoveObserver(IObserver observer);
+        void NotifyObservers();
+    }
+
+    public class NotificationCenter: IObservable
+    {
+        public List<IObserver> Observers { get; private set; }
+
+        public NotificationCenter()
+        {
+            Observers = new List<IObserver>();
+        }
+
+        public void AddObserver(IObserver observer)
+        {
+            if (observer == null)
+            {
+                throw new NullReferenceException(nameof(observer));
+            }
+
+            Observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            if (observer == null)
+            {
+                throw new NullReferenceException(nameof(observer));
+            }
+
+            Observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach(IObserver observer in Observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+
+    class Card: IObserver
+    {
+        public int Number { get; private set; }
+        public bool IsSwiped { get; private set; }
+
+        public Card(int number)
+        {
+            Number = number;
+            IsSwiped = false;
+        }
+
+        public void Swipe(int swipeDirection)
+        {
+            if (swipeDirection >= 0)
+            {
+                IsSwiped = true;
+            }
+        }
+
+        public void Update()
+        {
+            if (IsSwiped == true)
+            {
+                Console.WriteLine($"Card#{Number} has swiped!");
+            }
+            else
+            {
+                Console.WriteLine($"Card#{Number} hasn't swiped!");
+            }
+        }
+    }
+}
---
+++ Behavioral Patterns/Observer/CS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Observer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine();
+            NotificationCenter center = new NotificationCenter();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var card = new Card(i);
+                if (i > 2 && i % 2 == 0)
+                {
+                    card.Swipe(-3 + i);
+                }
+                center.AddObserver(card);
+            }
+
+            center.NotifyObservers();
+        }
+
+        // OUTPUT
+        // Card#0 hasn't swiped!
+        // Card#1 hasn't swiped!
+        // Card#2 hasn't swiped!
+        // Card#3 hasn't swiped!
+        // Card#4 has swiped!
+        // Card#5 hasn't swiped!
+        // Card#6 has swiped!
+        // Card#7 hasn't swiped!
+        // Card#8 has swiped!
+        // Card#9 hasn't swiped!
+    }
+}
