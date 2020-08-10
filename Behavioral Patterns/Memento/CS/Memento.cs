41ca1f61cc59c97805b9f67ead87bb9e094638c9
Timonin Anton <dotruger.37@gmail.com>
2020-08-10 11:28:01 +0300

    Add C# realization

---
+++ Behavioral Patterns/Memento/CS/Memento.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Memento
+{
+    public struct Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"Point({X}, {Y})";
+        }
+    }
+
+    // OBJECT DRAING
+    public class Drawing
+    {
+        public List<Point> points { get; private set; }
+
+        public Drawing()
+        {
+            this.points = new List<Point>(); 
+        }
+
+        public void Add(Point point)
+        {
+            this.points.Add(point);
+        }
+
+        public MementoDrawing SaveState()
+        {
+            return new MementoDrawing(this);
+        }
+
+        public void RestoreState(MementoDrawing newState) {
+            this.points.Clear();
+
+            foreach (Point point in newState.Points)
+            {
+                points.Add(point);
+            }
+        }
+    }
+
+    // MEMENTO
+    public class MementoDrawing
+    {
+        public List<Point> Points { get; private set; }
+
+        public MementoDrawing(Drawing drawing)
+        {
+            this.Points = new List<Point>();
+
+            foreach (Point point in drawing.points)
+            {
+                Points.Add(point);
+            } 
+        }
+
+        public override string ToString()
+        {
+            string text = "MementoDrawing\n";
+            foreach(var item in this.Points)
+            {
+                text += $"{item.ToString()}\n";
+            }
+            text += "\n";
+
+            return text;
+        }
+    }
+
+    // CARETAKER
+    public class HistoryDrawing
+    {
+        public Stack<MementoDrawing> Drawings { get; private set; }
+
+        public HistoryDrawing()
+        {
+            this.Drawings = new Stack<MementoDrawing>();
+        }
+
+        public void Push(MementoDrawing drawing)
+        {
+            this.Drawings.Push(drawing);
+        }
+
+        public MementoDrawing Pop()
+        {
+            return this.Drawings.Pop();
+        }
+    }
+}
---
+++ Behavioral Patterns/Memento/CS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Memento
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            HistoryDrawing history = new HistoryDrawing();
+            Drawing drawing1 = new Drawing();
+
+            drawing1.Add(new Point(3, 4));
+            drawing1.Add(new Point(-1, 2));
+            drawing1.Add(new Point(10, 6));
+
+            Drawing drawing2 = new Drawing();
+
+            drawing2.Add(new Point(0, 1));
+            drawing2.Add(new Point(1, 0));
+            drawing2.Add(new Point(2, 1));
+
+            history.Push(drawing1.SaveState());
+            history.Push(drawing2.SaveState());
+
+            Console.WriteLine(history.Pop().ToString());
+            Console.WriteLine(history.Pop().ToString());
+
+            // OUTPUT
+            // MementoDrawing
+            // Point(0, 1)
+            // Point(1, 0)
+            // Point(2, 1)
+
+            // MementoDrawing
+            // Point(3, 4)
+            // Point(-1, 2)
+            // Point(10, 6)
+        }
+    }
+}
