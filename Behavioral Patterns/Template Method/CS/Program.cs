ab8307f83a38ea977b217821fc8f46f431b846b1
Timonin Anton <dotruger.37@gmail.com>
2020-08-11 13:12:48 +0300

    Add C# realization Template Method

---
+++ Behavioral Patterns/Template Method/CS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            FullScreenForm form1 = new FullScreenForm("Form1", "1024x780");
+            form1.LoadScreen();
+            Console.WriteLine();
+            Console.WriteLine(form1.Log);
+        }
+
+        // OUTPUT
+        // Form1 -> 1024x780
+        // Form1 :- LoadForm
+        // Form1 :- FormDidLoad
+        // Form1 :- FormWillAppear
+        // Form1 :- FormDidAppear
+    }
+}
---
+++ Behavioral Patterns/Template Method/CS/TemplateMethod.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace TemplateMethod
+{
+    public abstract class FormLoadTemplateMethod
+    {
+        public string NameForm { get; private set; }
+        public string Frame { get; private set; }
+        public string Log { get; protected set; }
+
+        public FormLoadTemplateMethod(string nameForm, string frame)
+        {
+            NameForm = nameForm;
+            Frame = frame;
+            Log = "";
+        }
+
+        public void LoadScreen()
+        {
+            Log = $"{NameForm} -> {Frame}\n";
+            LoadForm();
+            FormDidLoad();
+            FormWillAppear();
+            FormDidAppear();
+        }
+
+        protected abstract void LoadForm();
+        protected abstract void FormDidLoad();
+        protected abstract void FormWillAppear();
+        protected abstract void FormDidAppear();
+    }
+
+    public class FullScreenForm: FormLoadTemplateMethod
+    {
+        public FullScreenForm(string nameForm, string frame) : base(nameForm, frame) { }
+
+        protected override void LoadForm()
+        {
+            Log += $"{NameForm} :- LoadForm\n";
+        }
+
+        protected override void FormDidLoad()
+        {
+            Log += $"{NameForm} :- FormDidLoad\n";
+        }
+
+        protected override void FormWillAppear()
+        {
+            Log += $"{NameForm} :- FormWillAppear\n";
+        }
+
+        protected override void FormDidAppear()
+        {
+            Log += $"{NameForm} :- FormDidAppear\n\n";
+        }
+    }
+}
