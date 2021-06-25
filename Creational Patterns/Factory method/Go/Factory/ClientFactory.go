43481cc19378a902f6e7abafacb325f2055b240b
Timonin Anton <dotruger.37@gmail.com>
2021-06-25 13:45:30 +0300

    Добавить паттерн фабрика для golang

---
+++ Creational Patterns/Factory method/Go/Factory/ClientFactory.go
@@ -0,0 +1,41 @@
+package factory
+
+import (
+	"log"
+	"net/http"
+)
+
+type DoRequest interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+// MockHttpClient
+type MockHTTPClient struct{}
+
+func NewMockHTTPClient() DoRequest {
+	return MockHTTPClient{}
+}
+
+func (c MockHTTPClient) Do(req *http.Request) (*http.Response, error) {
+	return &http.Response{}, nil
+}
+
+// HttpClient
+type SimpleHTTPClient struct{}
+
+func NewSimpleHTTPClient() DoRequest {
+	return SimpleHTTPClient{}
+}
+
+func (c SimpleHTTPClient) Do(req *http.Request) (*http.Response, error) {
+	var client http.Client
+	resp, err := client.Get("http://golang.org/")
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	log.Println(resp)
+	return resp, nil
+}
